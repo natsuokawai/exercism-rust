07c20cfb97584b0e0b75011b602ea92292bcce6d
natsuokawai <kawai.natsuo@gmail.com>
2021-02-27 10:59:17 +0900

    Modify Leap

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,3 +1,8 @@
 pub fn is_leap_year(year: u64) -> bool {
-    year % 4 == 0 || year % 400 == 0 && year % 100 == 0
+    match (year % 4, year % 100, year % 400) {
+        (0, 0, 0) => true,
+        (0, 0, _) => false,
+        (0, _, _) => true,
+        (_, _, _) => false,
+    }
 }
