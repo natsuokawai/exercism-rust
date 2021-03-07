ca9698803175792a88f400416ce2e5068b0b4f54
natsuokawai <kawai.natsuo@gmail.com>
2021-03-07 23:15:32 +0900

    Modify Nth prime

--- nth-prime/src/lib.rs
+++ nth-prime/src/lib.rs
@@ -1,35 +1,11 @@
-pub fn nth(n: u32) -> u32 {
-    let mut idx = 0;
-    let mut res: u32 = 0;
-    while idx <= n {
-        if is_prime(res) {
-            if idx == n {
-                return res;
-            }
-            idx += 1;
-        }
-        res += 1;
-    }
-
-    res
+pub fn is_prime(n: u32) -> bool {
+	match n {
+		1 => false,
+		2 => true,
+   		_ => !(2..n - 1).any(|i| n % i == 0),
+	}
 }
 
-fn is_prime(n: u32) -> bool {
-    if n == 0 {
-        return false;
-    }
-    if n == 1 {
-        return false;
-    }
-    if n == 2 {
-        return true;
-    }
-
-    for num in 2..=(n / 2) {
-        if n % num == 0 {
-            return  false;
-        }
-    }
-
-    true
+pub fn nth(n: u32) -> u32 {
+	(1..).filter(|c| is_prime(*c)).nth(n as usize).unwrap()
 }
