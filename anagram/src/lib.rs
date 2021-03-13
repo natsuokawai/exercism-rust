147a42cc1d099bfc2fb9174d67c105b28787e9ed
natsuokawai <kawai.natsuo@gmail.com>
2021-03-13 22:13:16 +0900

    Modify anagram for perfomance improvement

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -2,25 +2,23 @@ use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     let mut res: HashSet<&'a str> = HashSet::new();
-    let left = word.to_lowercase();
-    for s in possible_anagrams {
-        let rignt = s.to_lowercase();
-        if left != rignt && compare_chars(&left, &rignt) {
-            res.insert(s);
+    let word_lower = word.to_lowercase();
+    let word_sorted = sort_char(&word_lower);
+
+    for pa in possible_anagrams {
+        let pa_lower = pa.to_lowercase();
+        if pa.len() == word_lower.len()
+            && pa_lower != word_lower
+            && word_sorted == sort_char(&pa_lower)
+        {
+            res.insert(pa);
         }
     }
     res
 }
 
-fn compare_chars(left: &str, right: &str) -> bool {
-    let mut l = left.chars().collect::<Vec<_>>();
-    let mut r = right.chars().collect::<Vec<_>>();
-    l.sort();
-    r.sort();
-
-    if l == r {
-        return true;
-    }
-
-    false
+fn sort_char(c: &str) -> Vec<char> {
+    let mut res: Vec<char> = c.chars().collect();
+    res.sort_unstable();
+    res
 }
