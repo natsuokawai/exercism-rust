c8b41aab7cf351997ad50186e83545b33a797c73
natsuokawai <kawai.natsuo@gmail.com>
2021-03-18 09:12:43 +0900

    Fix minesweeper for edge cases

--- minesweeper/src/lib.rs
+++ minesweeper/src/lib.rs
@@ -13,7 +13,6 @@ pub fn annotate(minefield: &[&str]) -> Vec<String> {
     for row in 0..row_len {
         let mut row_str = String::new();
         for col in 0..col_len {
-
             if minefield[row].chars().nth(col).unwrap() == '*' {
                 row_str += "*";
                 continue;
@@ -21,11 +20,11 @@ pub fn annotate(minefield: &[&str]) -> Vec<String> {
 
             let mut count = 0;
             let r_start = cmp::max(0, (row as i32) - 1) as usize;
-            let r_end = cmp::min((row + 1) as i32, row_len as i32) as usize;
-            for r in r_start..r_end {
+            let r_end = cmp::min((row + 1) as i32, (row_len - 1) as i32) as usize;
+            for r in r_start..=r_end {
                 let c_start = cmp::max(0, (col as i32) - 1) as usize;
-                let c_end = cmp::min((col + 1) as i32, col_len as i32) as usize;
-                for c in c_start..c_end {
+                let c_end = cmp::min((col + 1) as i32, (col_len - 1) as i32) as usize;
+                for c in c_start..=c_end {
                     if minefield[r].chars().nth(c).unwrap() == '*' {
                         count += 1;
                     }
