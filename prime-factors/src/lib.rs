58ef3935b2f05aea9af10157287df4d142c686a1
natsuokawai <kawai.natsuo@gmail.com>
2021-03-20 08:53:57 +0900

    Refactor prime-factors

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,10 +1,10 @@
 pub fn factors(n: u64) -> Vec<u64> {
+    let mut result = Vec::new();
     let mut n_dup = n.clone();
-    let mut res: Vec<u64> = vec![];
 
     for num in 2..=n {
         while n_dup % num == 0 {
-            res.push(num);
+            result.push(num);
             n_dup /= num;
         }
         if n_dup == 1 {
@@ -12,5 +12,5 @@ pub fn factors(n: u64) -> Vec<u64> {
         }
     }
 
-    res
+    result
 }
